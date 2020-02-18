4b6f485f56d9780b96f1b4d1441630882cf2da5e
AKR0SS <zachary192837465@gmail.com>
2020-02-18 15:00:52 -0500

    checks if bot is in vc

--- commands/skip.js
+++ commands/skip.js
@@ -1,7 +1,20 @@
 module.exports = {
     name: 'skip',
     description: 'skips the video currently playing',
-    execute(message, servers) {
+    execute(message, servers, client) {
+        let VoiceChannel = message.guild.channels.find(channel => channel.id === message.member.voiceChannelID);
+        
+        // Checks if user is in a voice channel
+        if(!VoiceChannel) {
+            message.channel.send('How about you try joining a vc first ya faggot? :)')
+            return;
+        }
+        // Checks if bot is in voice channel
+        else if(VoiceChannel !== client.voiceChannelID) {
+            message.channel.send('Try joining me and then skipping a song nerd :)')
+            return;
+        }
+
         var server = servers[message.guild.id];
 
         if(server.dispatcher) {
