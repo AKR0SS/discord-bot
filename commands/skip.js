f4ac66c31bcc03479e8e2321303b5a1231279f49
AKR0SS <zachary192837465@gmail.com>
2020-02-17 20:22:22 -0500

    Initial Commit

--- commands/skip.js
+++ commands/skip.js
@@ -4,7 +4,10 @@ module.exports = {
     execute(message, servers) {
         var server = servers[message.guild.id];
 
-        if(server.dispactacher) 
+        if(server.dispatcher) {
+            // bc of dispatcher, it ends() current playing song
             server.dispatcher.end();
+            message.channel.send('why did you queue that song if you were just going to skip it faggot?')
+        }
     }
 }
