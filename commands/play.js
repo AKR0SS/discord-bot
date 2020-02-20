4162ae4cddb15b21a873a33b938f7a6093d825a7
AKR0SS <zachary192837465@gmail.com>
2020-02-20 13:29:56 -0500

    Link check
    this is probably a really really bad way of checking if it's a youtube link

--- commands/play.js
+++ commands/play.js
@@ -1,10 +1,12 @@
 const ytdl = require("ytdl-core");
+const Discord = require('discord.js');
+const { version } = require('F:/Programming/discord-bot/config.json');
 
 module.exports = {
     name: 'play',
     description: 'plays music from youtube',
     alias: 'p',
-    execute(message, args, servers) {
+    execute(message, args, servers, client, prefix) {
         // VoiceChannel gets the user's current voice channel
         let VoiceChannel = message.guild.channels.find(channel => channel.id === message.member.voiceChannelID);
 
@@ -19,45 +21,82 @@ module.exports = {
             return;
         }
         
-        // Sets a server's music queue
-        if(!servers[message.guild.id]) servers[message.guild.id] = {
-            queue: []
-        };
+        if (message.content.startsWith(prefix + 'play https://www.youtube.com/watch?v=') || message.content.startsWith(prefix + 'play |play https://youtu.be/')) {
+            // Sets a server's music queue
+            if(!servers[message.guild.id]) servers[message.guild.id] = {
+                queue: []
+            };
 
-        // Sets url to a string
-        var server = servers[message.guild.id];
-        server.queue.push(args.join(" "));
-
-        // Joins the user's voice chat and calls play()
-        if(!message.guild.voiceConnection) VoiceChannel.join().then(function(connection) {
-            play(connection, message);
-        });
-
-        // Called by execute
-        function play(connection, message) {
+            // Sets url to a string
             var server = servers[message.guild.id];
+            server.queue.push(args.join(" "));
 
-            // plays the video in queue and passes settings args
-            server.dispatcher = connection.playStream(ytdl(server.queue[0]), {
-                filter: "audioonly",
-                quality: 'highestaudio',
-                volume: 1,
-                highWaterMark: 1<<25
+            // Joins the user's voice chat and calls play()
+            if(!message.guild.voiceConnection) VoiceChannel.join().then(function(connection) {
+                play(connection, message);
             });
-            
-            server.queue.shift();
 
-            server.dispatcher.on('end', function() {
-                // Checks if songs are queue'd
-                if(server.queue[0]) {
-                    message.channel.send('-> Playing the next dumbass song in queue :)')
-                    play(connection, message)
+            // Checks if it's the first song being queue'd and sends a now playing message
+            if(server.queue[1]) {
+                nowPlaying(message)
+            }
+
+            // Called by execute
+            function play(connection, message) {
+                var server = servers[message.guild.id];
+
+                // plays the video in queue and passes settings args
+                server.dispatcher = connection.playStream(ytdl(server.queue[0], {
+                    filter: "audioonly",
+                    highWaterMark: 1<<25
+                }));
+                
+                server.queue.shift();
+
+                server.dispatcher.on('end', function() {
+                    // Checks if songs are queue'd
+                    if(server.queue[0]) {
+                        nowPlaying(message)
+                        
+                        play(connection, message)
+                    }
+                    else {
+                        message.channel.send('-> Queue is now empty, fill me again ;)')
+                        connection.disconnect();
+                    }
+                });
+            }
+        }
+        else if(!message.content.startsWith('|play https://www.youtube.com/watch?v=')) {
+            message.channel.send('not a video')
+            return;
+        }
+        else if (!message.content.startsWith('|play https://youtu.be/')) {
+            message.channel.send('not a video')
+            return;
+        }
+        else
+            return;
+
+        // Sends now playing message
+        function nowPlaying(message) {
+            message.channel.send({embed: {
+                color: 16102856,
+                thumbnail: {
+                    url: ytdl.getInfo.thumbnail // idk how to get a thumbnail
+                },
+                fields: [
+                    {
+                        name: 'Now Playing',
+                        value: 'TITLE'//ytdl.getBasicInfo()
+                    }
+                ],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: client.user.avatarURL,
+                    text: 'v' + version
                 }
-                else {
-                    message.channel.send('-> Queue is now empty, fill me again ;)')
-                    connection.disconnect();
-                }
-            });
+            }});
         }
     }
 }
