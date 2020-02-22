e47c55322f195aa8a539c09243f8c670cb9d6655
AKR0SS <zachary192837465@gmail.com>
2020-02-22 00:12:30 -0500

    switched to a try catch

--- commands/join.js
+++ commands/join.js
@@ -4,21 +4,20 @@ module.exports = {
     execute(message, client) {
         let VoiceChannel = message.guild.channels.find(channel => channel.id === message.member.voiceChannelID);
     
-        // Checks if user is in a voice chat
         if(!VoiceChannel) {
             message.channel.send('How about you try joining a vc first ya faggot? :)')
             return;
         }
 
-        if(!message.guild.voiceConnection)  {
+        try {
             VoiceChannel.join()
 
             message.channel.send(message.author + " is a simp feeling lonely without a girlfriend so they've got a bot to join 'em, me :)")
 
             return;
         }
-        else {
-            message.channel.send('but.. you said to join and I did.... fuking simp :)')
+        catch {
+            message.channel.send("I.. I, don't know what to do with myself sorry :(")
         }
     }
 }
