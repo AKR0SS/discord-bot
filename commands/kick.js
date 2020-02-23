7d9ddde6df4bb24486011a18121def260c58ea84
AKR0SS <zachary192837465@gmail.com>
2020-02-22 20:13:02 -0500

    file path

--- commands/kick.js
+++ commands/kick.js
@@ -1,5 +1,4 @@
-const Discord = require('discord.js');
-const { version } = require('F:/Programming/discord-bot/config.json');
+const { version } = require('../config.json');
 
 module.exports  = {
 	name: 'kick',
