f620273cc23a8ebfe96a08ee56f8d4d2f113431f
AKR0SS <zachary192837465@gmail.com>
2020-02-25 21:38:06 -0500

    Initial Commit
    doesn't.. actually work rn..

---
+++ commands/mute.js
@@ -0,0 +1,36 @@
+const { version } = require('../config.json');
+
+module.exports = {
+    name: 'mute',
+    description: 'either mutes or temp mutes @user',
+    execute(message, args, client) {
+        let muteUser = message.guild.member(message.mentions.users.first());
+        let muteReason = args.slice(1).join(' ');
+
+        if (!muteUser) {
+            message.channel.send("That's not even a user, what am i paying you to be admin for cuck? :)")
+            return;
+        }
+        
+        muteEmbed(muteReason)
+
+        function muteEmbed(muteReason) {
+            const taggedUser = message.mentions.users.first();
+
+            message.channel.send({embed: {
+				color: 16102856,
+                fields: [
+                    {
+                        name: `User: ${taggedUser.username}`,
+                        value: 'Reason: ' + muteReason
+                    },
+                ],
+                timestamp: new Date(),
+                footer: {
+                    icon_url: client.user.avatarURL,
+                    text: 'v' + version
+                }
+            }});
+        }
+    }
+}
